3b9c7361ea675b646df2eccb909be02c32c5f219
Bastien Ricard <bastien.ricard-dev@outlook.com>
2025-01-13 20:44:37 +0100

    Add hero section

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="sticky top-0 backdrop-blur-sm">
+    <header className="sticky top-0 backdrop-blur-sm z-20">
       <div className="py-3 flex justify-center items-center gap-3 bg-black text-white text-sm">
         <p className="max-md:hidden text-white/60">
           Streamline your workflow and boost your productivity.
