d6f53d84c25aa61cfdf9784032bb7f8e10a91515
Bastien Ricard <bastien.ricard-dev@outlook.com>
2025-01-13 20:56:38 +0100

    Add logo ticker section

---
+++ src/sections/logo-ticker.tsx
@@ -0,0 +1,50 @@
+import acmeLogo from '@/assets/logo-acme.png';
+import apexLogo from '@/assets/logo-apex.png';
+import celestialLogo from '@/assets/logo-celestial.png';
+import echoLogo from '@/assets/logo-echo.png';
+import pulseLogo from '@/assets/logo-pulse.png';
+import quantumLogo from '@/assets/logo-quantum.png';
+import Image from 'next/image';
+
+export default function LogoTicker() {
+  return (
+    <section className="py-8 md:py-12 bg-white">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image
+              src={acmeLogo}
+              alt="Acme Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={quantumLogo}
+              alt="Quantum Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={echoLogo}
+              alt="Echo Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={celestialLogo}
+              alt="Celestial Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={pulseLogo}
+              alt="Pulse Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={apexLogo}
+              alt="Apex Logo"
+              className="logo-ticker-image"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
