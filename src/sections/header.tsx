b1c4847ed6c270ed39fe4783281af20a15d0640f
Bastien Ricard <bastien.ricard-dev@outlook.com>
2025-01-13 17:44:33 +0100

    Add btn class and blur background to header

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -1,12 +1,12 @@
-import ArrowRight from '@/assets/arrow-right.svg';
-import Logo from '@/assets/logosaas.png';
-import MenuIcon from '@/assets/menu.svg';
+import arrowRight from '@/assets/arrow-right.svg';
+import logo from '@/assets/logosaas.png';
+import menuIcon from '@/assets/menu.svg';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="sticky top-0">
+    <header className="sticky top-0 backdrop-blur-sm">
       <div className="py-3 flex justify-center items-center gap-3 bg-black text-white text-sm">
         <p className="max-md:hidden text-white/60">
           Streamline your workflow and boost your productivity.
@@ -14,7 +14,7 @@ export default function Header() {
         <div className="inline-flex gap-1 items-center">
           <p>Get started for free</p>
           <Image
-            src={ArrowRight}
+            src={arrowRight}
             alt="Arrow right"
             width={16}
             height={16}
@@ -25,9 +25,9 @@ export default function Header() {
       <div className="py-5">
         <div className="container">
           <div className="flex justify-between items-center">
-            <Image src={Logo} alt="SaaS Logo" width={40} height={40} />
+            <Image src={logo} alt="SaaS Logo" width={40} height={40} />
             <Image
-              src={MenuIcon}
+              src={menuIcon}
               alt="Menu Icon"
               width={20}
               height={20}
@@ -39,9 +39,7 @@ export default function Header() {
               <Link href="#">Customers</Link>
               <Link href="#">Updates</Link>
               <Link href="#">Help</Link>
-              <button className="px-4 py-2 inline-flex justify-center items-center rounded-lg bg-black font-medium text-white tracking-tight">
-                Get for free
-              </button>
+              <button className="btn btn-primary">Get for free</button>
             </nav>
           </div>
         </div>
